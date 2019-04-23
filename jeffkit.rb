bfe9129228d03cf3317195dc5a7654a9cf4d90c7
Jeff Cedilla <jeff.cedilla@engageft.com>
2019-04-23 13:35:50 -0700

    change formula

--- jeffkit.rb
+++ jeffkit.rb
@@ -1,7 +1,7 @@
 class JeffKit < Formula
     desc "A command line tool for convenience"
     homepage "https://github.com/spaghetticoderapps/JeffKit"
-    url "https://github.com/spaghetticoderapps/JeffKit/raw/master/archive/test.tar.gz"
+    url "https://github.com/spaghetticoderapps/homebrew-jeffkit/blob/master/archive/test.tar.gz"
     sha256 "a180816385656d36cd50c93a4e73cd4a3ed6209d99315a13ccae8bb2e2cc50c2"
     version "1.0.0"
     
