a6dc50396006f41da24e8e02b8f285481328ba41
Jeff Cedilla <jeff.cedilla@engageft.com>
2019-04-23 14:05:39 -0700

    revert version deletion

--- jeffkit.rb
+++ jeffkit.rb
@@ -3,6 +3,7 @@ class Jeffkit < Formula
     homepage "https://github.com/spaghetticoderapps/JeffKit"
     url "https://github.com/spaghetticoderapps/JeffKit/raw/master/archive/test.tar.gz"
     sha256 "a180816385656d36cd50c93a4e73cd4a3ed6209d99315a13ccae8bb2e2cc50c2"
+    version "1.0.0"
     
     bottle :unneeded
     
