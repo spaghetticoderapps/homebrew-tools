b4c89e33728c5c452ac2c7137726505183dd6aa6
Jeff Cedilla <jeff.cedilla@engageft.com>
2019-04-23 14:12:48 -0700

    change tar

--- jeffkit.rb
+++ jeffkit.rb
@@ -1,8 +1,8 @@
 class Jeffkit < Formula
     desc "A command line tool for convenience"
     homepage "https://github.com/spaghetticoderapps/JeffKit"
-    url "https://github.com/spaghetticoderapps/JeffKit/raw/master/archive/test.tar.gz"
-    sha256 "a180816385656d36cd50c93a4e73cd4a3ed6209d99315a13ccae8bb2e2cc50c2"
+    url "https://github.com/spaghetticoderapps/JeffKit/raw/master/archive/jeffkit.tar.gz"
+    sha256 "79e98dca790a6a0e360d38b80d47e5cec45a15e74e185b0d2e041f85800a464b"
     version "1.0.0"
     
     bottle :unneeded
