c668b0d1276b5e243474cf832585ebdbbfeb524e
Jeff Cedilla <jeff.cedilla@engageft.com>
2019-04-23 13:32:48 -0700

    change sha

--- jeffkit.rb
+++ jeffkit.rb
@@ -1,8 +1,8 @@
 class JeffKit < Formula
     desc "A command line tool for convenience"
     homepage "https://github.com/spaghetticoderapps/JeffKit"
-    url "https://github.com/spaghetticoderapps/JeffKit/raw/master/archive/weather-1.0.0.tar.gz"
-    sha256 "b1c7ab25dfb4530a5e35aa690d79469de5ec419dd284f03868935c2417e1ee3a"
+    url "https://github.com/spaghetticoderapps/JeffKit/raw/master/archive/test.tar.gz"
+    sha256 "a180816385656d36cd50c93a4e73cd4a3ed6209d99315a13ccae8bb2e2cc50c2"
     version "1.0.0"
     
     bottle :unneeded
